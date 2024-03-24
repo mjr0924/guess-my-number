301d3742fedc8452936207b6003fed0968e8f05c
Mohammad Javad Rezaei <mjr0924@gmail.com>
2024-03-24 21:34:05 +0330

    add npm & tailwindcss to project and add html concepts

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        base: ['"Press Start 2P"', "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
